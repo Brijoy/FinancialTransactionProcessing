2575c7fd43a7331436a6dae9cb061388bd7a27ca
Brijoy <brijoydmello@gmail.com>
2025-01-03 15:10:22 +0530

    FinancialTransactionProcessing

--- notification-serivce/notification-serivce/src/main/java/com/td/notification_serivce/dto/UserDTO.java
+++ notification-serivce/notification-serivce/src/main/java/com/td/notification_serivce/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.td.notification_serivce.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,13 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
     private Long id;
+    @NotEmpty(message = "User name should not be null or empty")
+    //@NotNull
     private String name;
+    @NotEmpty(message = "User email should not be null or empty")
+    @Email(message = "Email address should be valid")
     private String email;
+    @NotEmpty(message = "User phoneNumber should not be null or empty")
     private String phoneNumber;
 
 }
